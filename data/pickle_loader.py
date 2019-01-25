c575ff8f6aa0479c2b53f16328e138a803bc81b9
Yihan Kim <kabi@kaist.ac.kr>
2019-01-25 15:34:14 +0900

    remove cmu-wafer dataset, use pkl instead

--- data/pickle_loader.py
+++ data/pickle_loader.py
@@ -1,18 +1,25 @@
+'''
+2019. 1. 25
+Eojin Rho 
+
+import cmu-wafer
+'''
+
 import pickle as pkl
 import numpy as np
 import os
 
 dir = os.path.dirname(os.path.realpath(__file__))
 
-def np2pkl(file_name, data):
-    file_path = os.path.join(dir, file_name)
+def np2pkl(file_name, data, dataset_name = "cmu-wafer"):
+    file_path = os.path.join(dir, dataset_name, file_name)
     file_object = open(file_path, 'wb')
     pkl.dump(data, file_object)
     file_object.close()
     return None
 
-def pkl2np(file_name):
-    file_path = os.path.join(dir, file_name)
+def pkl2np(file_name, dataset_name = "cmu-wafer"):
+    file_path = os.path.join(dir, dataset_name, file_name)
     print(file_path)
     file_object = open(file_path, 'rb')
     data = pkl.load(file_object)
